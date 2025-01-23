190c19e5060ae9bc1da9a7762f00a03b81e6a1a5
AntonyanGev <138504476+AntonyanGev@users.noreply.github.com>
2025-01-23 12:44:41 +0400

    Update styles.js

--- objector/frontend/src/components/main/styles.js
+++ objector/frontend/src/components/main/styles.js
@@ -3,42 +3,39 @@ import styled from "styled-components";
 import { media } from "../brakePoint/mediaQuery";
 
 export const MainContainer = styled.div`
-  margin-top: 5rem;
+  margin: 5rem auto;
   display: flex;
   flex-direction: column;
+  align-items: center;
   text-align: center;
   overflow-x: hidden;
-  align-items: center;
+  padding: 0 1rem;
 
   ${media(
     "md",
     `
-     margin-top: 4.1875rem;
+    margin-top: 4.2rem;
     flex-direction: row;
     justify-content: space-between;
-    text-align: center;
-    align-items: center;
-
-    margin-left: 6rem;
-    margin-right: 6rem;
-  `
+    padding: 0 6rem;
+   `
   )};
 `;
 
 export const TextAndButtonsContainer = styled.div`
   max-width: 50rem;
-  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   text-align: center;
-  justify-content: center;
   margin: auto;
+
   ${media(
     "md",
     `
-    text-align: start;
-  `
+   text-align: start;
+    align-items: flex-start;
+   `
   )};
 `;
 
@@ -49,147 +46,96 @@ export const TextContainer = styled.div`
 `;
 
 export const ObjectContent = styled.h1`
-  font-size: 4.375rem;
-  align-items: center;
-  font-weight: 500;
+  font-size: clamp(2.5rem, 5vw, 6.375rem);
+  font-weight: 600;
   margin: 0;
-
-  ${media(
-    "md",
-    `
-    font-size: 5.375rem;
-    font-weight: 600;
-  `
-  )};
-
-  ${media(
-    "xl",
-    `
-   font-size: 9.375rem;
-  `
-  )};
 `;
 
 export const Title = styled.p`
-  font-size: 1rem;
+  font-size: clamp(0.8rem, 1.2vw, 1.5rem);
   margin: 0;
   text-align: center;
-  ${media(
-    "md",
-    `
-    text-align: end;
-  `
-  )};
-`;
-export const Description = styled.div`
-  color: rgba(255, 255, 255, 0.75);
-  padding: 1rem 2.7rem;
-  font-size: 0.8rem;
-  font-weight: 300;
-  line-height: 1.5rem;
-  text-align: center;
-  ${media(
-    "sm",
-    `
-    max-width: 23.875rem;
-    display: flex;
-    padding: 0;
-    text-align: start;
-    justify-content: center;
-    width: 100%;
-  `
-  )};
 
   ${media(
     "md",
     `
-    max-width: 20.875rem;
-  `
+text-align: start;
+   `
   )};
-  ${media(
-    "xl",
-    `
+`;
+
+export const Description = styled.div`
+  color: rgba(255, 255, 255, 0.75);
+  padding: 1rem;
+  font-size: clamp(0.8rem, 1vw, 1rem);
+  font-weight: 300;
+  line-height: 1.5rem;
+  text-align: center;
   max-width: 34.875rem;
-  `
+  margin: 0 auto;
+
+  ${media(
+    "md",
+    `
+  padding: 0;
+    text-align: start;
+   `
   )};
 `;
 
 export const MobileImageContainer = styled.div`
   margin: 1.25rem 0;
+
   ${media(
     "md",
     `
-   display: none;
-    margin: 0 auto;
-  `
+display: none;
+   `
   )};
 `;
 
 export const MobileImage = styled.img`
-  display: block;
   width: 100%;
-
-  ${media(
-    "md",
-    `
-   display: none;
-  `
-  )};
 `;
 
 export const DesktopImageContainer = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  display: flex;
+  display: none;
   justify-content: center;
   align-items: center;
-  overflow: hidden;
-`;
-
-export const LogoImage = styled.img`
-  display: none;
-  position: relative;
-  z-index: 1;
-  max-width: 100%;
+  flex-grow: 1;
 
   ${media(
     "md",
     `
-   display: block;
-    max-width: 29rem;
-    width: 100%;
-  `
+display: flex;
+   `
   )};
+`;
+
+export const LogoImage = styled.img`
+  max-width: 100%;
+  width: clamp(15rem, 50%, 29rem);
 `;
 
 export const ButtonContainer = styled.div`
   display: flex;
   margin-top: 2rem;
   gap: 0.5rem;
+  justify-content: center;
 
   ${media(
     "md",
     `
-   max-width: 21.375rem;
-   width:100%
-  
-  `
-  )};
-
-  ${media(
-    "xl",
-    `
- max-width: 34.875rem;
-  `
+   justify-content: flex-start;
+   `
   )};
 `;
 
 export const SignUpButton = styled.button`
   background-color: #fff;
-  padding: 1.2rem 4rem;
+  padding: 1rem 2rem;
   border-radius: 2rem;
-  font-size: 1rem;
+  font-size: clamp(0.8rem, 1vw, 1rem);
   font-weight: bold;
   cursor: pointer;
   border: none;
@@ -198,9 +144,9 @@ export const SignUpButton = styled.button`
 
 export const LogInButton = styled.button`
   background-color: #fff;
-  padding: 0.2rem 1rem;
+  padding: 1rem 1rem;
   border-radius: 2rem;
-  font-size: 1rem;
+  font-size: clamp(0.8rem, 1vw, 1rem);
   font-weight: bold;
   cursor: pointer;
   border: none;
@@ -211,16 +157,15 @@ export const PhoneButton = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 3.5rem;
-  height: 3.5rem;
+  width: 3rem;
+  height: 3rem;
   background-color: #fff;
   border-radius: 50%;
-  overflow: hidden;
   cursor: pointer;
 `;
 
 export const PhoneIcon = styled.img`
-  width: 1.1rem;
-  height: 1.1rem;
+  width: 1rem;
+  height: 1rem;
   object-fit: cover;
 `;
