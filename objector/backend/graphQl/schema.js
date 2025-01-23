b9cf9881d16c25cddd9c6073e1b3e24d4e29b10f
AntonyanGev <138504476+AntonyanGev@users.noreply.github.com>
2025-01-23 13:25:16 +0400

    Update schema.js

--- objector/backend/graphQl/schema.js
+++ objector/backend/graphQl/schema.js
@@ -14,18 +14,11 @@ email:String!
 password:String!
 }
 
-input changeUser{
-_id:ID!
-userName:String!
-email:String!
-password:String!
-}
 
 
 type authData{
 token:String!
 userId:String!
-
 }
 
 type Query{
@@ -34,9 +27,6 @@ login(email:String!, password:String!):authData
 
 type Mutation{
 addUser(add:inputUser!):user
-changeUsers(change:changeUser!):user
 }
-
-
 `);
 export default schema;
